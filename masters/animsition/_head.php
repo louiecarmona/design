ff16f0fdbfdb6cd1717d4b698d80a3e4c7a46e68
Louie Carmona <30575524+louiecarmona@users.noreply.github.com>
2019-01-03 20:38:52 +0400

    updated works

---
+++ masters/animsition/_head.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="testing website page">
+    <meta name="keyword" content="testing page">
+    <meta name="author" content="Louie Carmona">
+
+    <title>Test Template</title>
+
+    <!-- animation CSS -->
+    <link href="<?php echo WEBSITE.'public/test/css/style.css'; ?>" rel="stylesheet" />
+    <link href="<?php echo WEBSITE.'public/test/css/animsition.css'; ?>" rel="stylesheet" />
+
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
